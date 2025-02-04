761e4a0250c761ba1110a7d4a987d3fed068add9
florkae <138144836+Flaykky@users.noreply.github.com>
2025-02-05 00:03:11 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -13,11 +13,16 @@
 #ifdef _WIN32
 typedef SSIZE_T ssize_t;
 #include <windows.h>
-#include <wintun.h>
+
 #pragma comment(lib, "wintun.lib")
 #else
 #include <unistd.h>
 #endif
+
+#include <getopt.h>
+
+
+
 
 // Флаг для выхода из основного цикла
 static volatile bool terminate_flag = false;
@@ -30,13 +35,12 @@ void signal_handler(int signum) {
     }
 }
 
-// Функция для обработки входящих/исходящих данных
-void process_data(int socket_fd, int tun_fd) {
+void process_data(int socket_fd, Tunnel *tunnel) {
     char buffer[1024];
     ssize_t bytes_read;
 
     // Читаем данные из туннеля
-    bytes_read = read_from_tunnel(buffer, sizeof(buffer));
+    bytes_read = read_from_tunnel(tunnel, buffer, sizeof(buffer));
     if (bytes_read > 0) {
         // Шифруем данные
         size_t encrypted_len;
@@ -57,7 +61,7 @@ void process_data(int socket_fd, int tun_fd) {
         size_t decrypted_len;
         decrypt_data(buffer, bytes_read, buffer, &decrypted_len);
         // Записываем дешифрованные данные в туннель
-        write_to_tunnel(buffer, decrypted_len);
+        write_to_tunnel(tunnel, buffer, decrypted_len);
     } else if (bytes_read == 0) {
         log_info("Connection closed by peer");
         terminate_flag = true;
@@ -68,7 +72,45 @@ void process_data(int socket_fd, int tun_fd) {
 
 // Основная функция
 int main(int argc, char *argv[]) {
-    // Инициализация системы логирования
+    static struct option long_options[] = {
+        {"mode", required_argument, 0, 'm'},
+        {"server", required_argument, 0, 's'},
+        {"port", required_argument, 0, 'p'},
+        {"proxy", required_argument, 0, 'x'},
+        {"proxy-port", required_argument, 0, 'y'},
+        {0, 0, 0, 0}
+    };
+
+    const char *mode = "tcp";
+    const char *server_ip = "127.0.0.1";
+    int server_port = 8080;
+    const char *proxy_ip = NULL;
+    int proxy_port = 0;
+
+    int opt;
+    while ((opt = getopt_long(argc, argv, "m:s:p:x:y:", long_options, NULL)) != -1) {
+        switch (opt) {
+            case 'm':
+                mode = optarg;
+                break;
+            case 's':
+                server_ip = optarg;
+                break;
+            case 'p':
+                server_port = atoi(optarg);
+                break;
+            case 'x':
+                proxy_ip = optarg;
+                break;
+            case 'y':
+                proxy_port = atoi(optarg);
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [--mode tcp|udp] [--server ip] [--port port] [--proxy ip] [--proxy-port port]\n", argv[0]);
+                return EXIT_FAILURE;
+        }
+    }
+        // Инициализация системы логирования
     init_logging("vpn.log", LOG_LEVEL_INFO, true);
 
     // Обработка параметров командной строки
@@ -132,8 +174,14 @@ int main(int argc, char *argv[]) {
 
     log_info("Shutting down VPN core...");
 
+    Tunnel tunnel;
+    if (!setup_tunnel(&tunnel, socket_fd)) {
+    log_error("Failed to set up tunnel.");
+    return EXIT_FAILURE;
+}
+
     // Очистка ресурсов
-    teardown_tunnel();
+    teardown_tunnel(&tunnel);
     cleanup_encryption();
     close_connection(socket_fd);
 
