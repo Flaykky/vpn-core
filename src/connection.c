67d987a98f381c60ba43258a85d3ec64df6491c2
florkae <138144836+Flaykky@users.noreply.github.com>
2025-02-05 23:07:04 +0300

    Update connection.c

--- src/connection.c
+++ src/connection.c
@@ -6,7 +6,7 @@
 #include <pthread.h>
 
 
-#include "openssl/types.h"
+#include "types.h"
 
 #ifdef _WIN32
 #include <winsock2.h>
@@ -328,6 +328,87 @@ int establish_connection(const char *server_ip, int port) {
     return sockfd;
 }
 
+
+// Установка UDP-соединения
+int establish_udp_connection(const char *server_ip, int port) {
+    int sockfd;
+    struct sockaddr_in server_addr;
+
+    // Инициализация Winsock на Windows
+    init_winsock();
+
+    // Создание UDP-сокета
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+#ifdef _WIN32
+        log_error("Socket creation failed");
+        cleanup_winsock();
+#else
+        perror("Socket creation failed");
+#endif
+        return -1;
+    }
+
+    // Настройка адреса сервера
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port);
+
+    // Преобразование IP-адреса из текстового формата в сетевой
+    if (inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
+        log_error("Invalid address/ Address not supported");
+        close_connection(sockfd);
+        cleanup_winsock();
+        return -1;
+    }
+
+    log_info("UDP connection established successfully");
+    return sockfd;
+}
+
+// Отправка данных через UDP
+ssize_t send_udp_data(int socket_fd, const void *data, size_t length, const struct sockaddr_in *server_addr) {
+    if (!is_socket_valid(socket_fd)) {
+        log_error("Invalid socket descriptor");
+        return -1;
+    }
+
+    ssize_t bytes_sent = sendto(socket_fd, data, length, 0, (const struct sockaddr *)server_addr, sizeof(*server_addr));
+    if (bytes_sent < 0) {
+#ifdef _WIN32
+        log_error("Sendto failed");
+#else
+        perror("Sendto failed");
+#endif
+        return -1;
+    }
+
+    return bytes_sent;
+}
+
+// Получение данных через UDP
+ssize_t receive_udp_data(int socket_fd, void *buffer, size_t length, struct sockaddr_in *client_addr) {
+    if (!is_socket_valid(socket_fd)) {
+        log_error("Invalid socket descriptor");
+        return -1;
+    }
+
+    socklen_t addr_len = sizeof(*client_addr);
+    ssize_t bytes_received = recvfrom(socket_fd, buffer, length, 0, (struct sockaddr *)client_addr, &addr_len);
+    if (bytes_received < 0) {
+#ifdef _WIN32
+        log_error("Recvfrom failed");
+#else
+        perror("Recvfrom failed");
+#endif
+        return -1;
+    } else if (bytes_received == 0) {
+        log_info("Connection closed by peer");
+        return 0;
+    }
+
+    return bytes_received;
+}
 
 
 // Проверка состояния соединения
