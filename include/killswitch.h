dbb06de1b99ca155c9dd824cf1951c145aa1a75e
flayk <138144836+Flaykky@users.noreply.github.com>
2025-02-22 01:11:02 +0300

    Update killswitch.h

--- include/killswitch.h
+++ include/killswitch.h
@@ -1,7 +1,12 @@
 #ifndef KILLSWITCH_H
 #define KILLSWITCH_H
 
+#include <stdbool.h>
 
+// Функция для включения Kill Switch
+bool enable_kill_switch(void);
 
+// Функция для отключения Kill Switch
+bool disable_kill_switch(void);
 
-#endif
+#endif // KILLSWITCH_H
