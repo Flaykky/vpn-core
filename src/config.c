2845ba5000b2a1b3a334d1e856d54362e8f224e3
flayk <138144836+Flaykky@users.noreply.github.com>
2025-02-28 20:05:52 +0300

    Update config.c

--- src/config.c
+++ src/config.c
@@ -1,62 +1,108 @@
 #include "config.h"
+#include "logging.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <getopt.h>
 #include <pthread.h>
-#include <connection.h>
+#include "cJSON.h"
+
+#ifdef _WIN32
+#include <winsock.h>
+#include <winsock2.h>
+#else
+#include <arpa/inet.h>
+#endif
 
 
-// Глобальная структура для хранения конфигурации
-static Config config = {0};
+Config global_config = {0};
 static pthread_mutex_t config_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-// Функция для инициализации конфигурации
+bool read_config_file(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        log_error("Failed to open config file: %s", filename);
+        return false;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char *json_str = malloc(length + 1);
+    if (!json_str) {
+        log_error("Memory allocation failed");
+        fclose(file);
+        return false;
+    }
+
+    fread(json_str, 1, length, file);
+    json_str[length] = '\0';
+    fclose(file);
+
+    cJSON *root = cJSON_Parse(json_str);
+    free(json_str);
+    if (!root) {
+        log_error("Failed to parse JSON");
+        return false;
+    }
+
+    cJSON *protocol = cJSON_GetObjectItemCaseSensitive(root, "protocol");
+    cJSON *server_ip = cJSON_GetObjectItemCaseSensitive(root, "serverIp");
+    cJSON *server_port = cJSON_GetObjectItemCaseSensitive(root, "serverPort");
+    cJSON *login = cJSON_GetObjectItemCaseSensitive(root, "login");
+    cJSON *password = cJSON_GetObjectItemCaseSensitive(root, "password");
+
+    if (cJSON_IsString(protocol) && cJSON_IsString(server_ip) && cJSON_IsNumber(server_port)) {
+        global_config.protocol = strdup(protocol->valuestring);
+        global_config.server_ip = strdup(server_ip->valuestring);
+        global_config.server_port = server_port->valueint;
+        global_config.login = login ? strdup(login->valuestring) : NULL;
+        global_config.password = password ? strdup(password->valuestring) : NULL;
+    } else {
+        log_error("Invalid config format");
+        cJSON_Delete(root);
+        return false;
+    }
+
+    cJSON_Delete(root);
+    log_info("Configuration loaded from JSON file");
+    return true;
+}
+
 bool initialize_config(int argc, char *argv[]) {
     static struct option long_options[] = {
         {"server", required_argument, 0, 's'},
         {"port", required_argument, 0, 'p'},
-        {"udp", no_argument, 0, 'u'}, // Новый флаг для UDP
+        {"udp", no_argument, 0, 'u'},
         {0, 0, 0, 0}
     };
 
-    int opt;
-    int option_index = 0;
-
-    // Установка значений по умолчанию
     pthread_mutex_lock(&config_mutex);
-    config.server_ip = strdup("127.0.0.1");
-    config.port = 8080;
-    config.use_udp = false; // По умолчанию TCP
+    global_config.server_ip = strdup("127.0.0.1");
+    global_config.server_port = 8080;
+    global_config.use_udp = false;
     pthread_mutex_unlock(&config_mutex);
 
+    int opt;
+    int option_index = 0;
     while ((opt = getopt_long(argc, argv, "s:p:u", long_options, &option_index)) != -1) {
         switch (opt) {
             case 's':
                 pthread_mutex_lock(&config_mutex);
-                free(config.server_ip);
-                config.server_ip = strdup(optarg);
-                if (!is_valid_ip(config.server_ip)) {
-                    fprintf(stderr, "Invalid server IP address: %s\n", optarg);
-                    pthread_mutex_unlock(&config_mutex);
-                    return false;
-                }
+                free(global_config.server_ip);
+                global_config.server_ip = strdup(optarg);
                 pthread_mutex_unlock(&config_mutex);
                 break;
             case 'p':
                 pthread_mutex_lock(&config_mutex);
-                config.port = atoi(optarg);
-                if (config.port <= 0 || config.port > 65535) {
-                    fprintf(stderr, "Invalid port number: %d\n", config.port);
-                    pthread_mutex_unlock(&config_mutex);
-                    return false;
-                }
+                global_config.server_port = atoi(optarg);
                 pthread_mutex_unlock(&config_mutex);
                 break;
             case 'u':
                 pthread_mutex_lock(&config_mutex);
-                config.use_udp = true;
+                global_config.use_udp = true;
                 pthread_mutex_unlock(&config_mutex);
                 break;
             default:
@@ -69,50 +115,51 @@ bool initialize_config(int argc, char *argv[]) {
     return true;
 }
 
-// Функция для получения серверного IP-адреса
 const char* get_server_ip(void) {
     pthread_mutex_lock(&config_mutex);
-    const char *ip = config.server_ip;
+    const char *ip = global_config.server_ip;
     pthread_mutex_unlock(&config_mutex);
     return ip;
 }
 
-// Функция для получения порта сервера
 int get_port(void) {
     pthread_mutex_lock(&config_mutex);
-    int port = config.port;
+    int port = global_config.server_port;
     pthread_mutex_unlock(&config_mutex);
     return port;
 }
 
-// Функция для проверки использования UDP
 bool get_use_udp(void) {
     pthread_mutex_lock(&config_mutex);
-    bool use_udp = config.use_udp;
+    bool use_udp = global_config.use_udp;
     pthread_mutex_unlock(&config_mutex);
     return use_udp;
 }
 
-// Вспомогательная функция для проверки корректности IP-адреса
 bool is_valid_ip(const char *ip) {
     struct sockaddr_in sa;
+#ifdef _WIN32
+    return inet_pton(AF_INET, ip, &(sa.sin_addr)) == 1;
+#else
     struct sockaddr_in6 sa6;
-
     if (inet_pton(AF_INET, ip, &(sa.sin_addr)) == 1) {
-        return true; // IPv4
+        return true;
     } else if (inet_pton(AF_INET6, ip, &(sa6.sin6_addr)) == 1) {
-        return true; // IPv6
+        return true;
     }
     return false;
+#endif
 }
 
-// Функция для очистки конфигурации при завершении программы
 void cleanup_config(void) {
     pthread_mutex_lock(&config_mutex);
-    if (config.server_ip) {
-        free(config.server_ip);
-        config.server_ip = NULL;
-    }
+    free(global_config.protocol);
+    free(global_config.server_ip);
+    free(global_config.login);
+    free(global_config.password);
+    free(global_config.country);
+    free(global_config.city);
+    memset(&global_config, 0, sizeof(Config));
     pthread_mutex_unlock(&config_mutex);
     log_info("Configuration cleaned up successfully");
 }
