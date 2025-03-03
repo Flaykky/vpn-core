0b57da5cae4e9ec8e8dbaf6b02288eaeff5f9c6a
flayk <138144836+Flaykky@users.noreply.github.com>
2025-03-03 23:44:32 +0300

    Update killswitch.h

--- include/killswitch.h
+++ include/killswitch.h
@@ -2,6 +2,27 @@
 #define KILLSWITCH_H
 
 #include <stdbool.h>
+#include <basetsd.h>
+
+
+
+#ifdef _WIN32
+
+#ifndef FWPM_LAYER_ALE_AUTH_CONNECT_V4
+#define FWPM_LAYER_ALE_AUTH_CONNECT_V4 {0x59b4a28f, 0x7b5a, 0x4a5e, {0xb8, 0x4e, 0x9d, 0x7c, 0x7c, 0x8b, 0x0f, 0x4d}}
+#endif
+
+#ifndef FWPM_LAYER_OUTBOUND_TRANSPORT_V4
+#define FWPM_LAYER_OUTBOUND_TRANSPORT_V4 {0x59b4a28f, 0x7b5a, 0x4a5e, {0xb8, 0x4e, 0x9d, 0x7c, 0x7c, 0x8b, 0x0f, 0x4d}}
+#endif
+
+#endif
+
+
+typedef struct {
+    HANDLE engine_handle;
+    UINT64 filter_id; // Ключ правила
+} KillSwitchContext;
 
 // Функция для включения Kill Switch
 bool enable_kill_switch(void);
