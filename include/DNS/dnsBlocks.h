24a578f031a5f016f992c9c2d484302d89ca2545
flayk <138144836+Flaykky@users.noreply.github.com>
2025-03-26 22:27:16 +0300

    Update dnsBlocks.h

--- include/DNS/dnsBlocks.h
+++ include/DNS/dnsBlocks.h
@@ -6,16 +6,30 @@
 
 // Типы блокировок
 typedef enum {
-    BLOCK_NONE = 0,
-    BLOCK_ADS = 1,
-    BLOCK_TRACKERS = 2,
-    BLOCK_BOTH = 3
+    BLOCK_NONE      = 0,
+    BLOCK_ADS       = 1 << 0,
+    BLOCK_TRACKERS  = 1 << 1
 } BlockType;
 
-// Настройка DNS-блокировок
-bool dns_blocks_set(BlockType type);
+// Структура для блокировщика
+typedef struct {
+    char **blocklist_ads;    // Список доменов рекламы
+    size_t ads_count;        // Количество элементов в списке рекламы
+    char **blocklist_trackers; // Список трекеров
+    size_t trackers_count;   // Количество элементов в списке трекеров
+    pthread_mutex_t mutex;   // Потокобезопасность
+} DnsBlocker;
 
-// Восстановление оригинальных DNS-настроек
-bool dns_blocks_reset();
+// Инициализация блокировщика
+bool dns_blocks_init(DnsBlocker *blocker, BlockType type);
+
+// Проверка домена на блокировку
+bool dns_blocks_check_domain(DnsBlocker *blocker, const char *domain);
+
+// Проверка IP на блокировку
+bool dns_blocks_check_ip(DnsBlocker *blocker, const char *ip);
+
+// Деинициализация
+void dns_blocks_cleanup(DnsBlocker *blocker);
 
 #endif // DNS_BLOCKS_H
